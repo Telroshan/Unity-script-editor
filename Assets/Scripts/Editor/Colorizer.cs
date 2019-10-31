dccfd1e0b86f1210bc9cdfce169b6787d55ff5d2
Vincent <vichenzo-thebaud@hotmail.com>
2019-10-31 15:38:52 +0100

    Colorizer

---
+++ Assets/Scripts/Editor/Colorizer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class Colorizer : ScriptableWizard
+    {
+        private Color _color = Color.white;
+        
+        [MenuItem("Tools/Colorizer")]
+        public static void OpenColorizer()
+        {
+            DisplayWizard<Colorizer>("Colorizer");
+        }
+        
+        public void OnGUI()
+        {
+            _color = EditorGUILayout.ColorField(_color);
+            if (GUILayout.Button("Colorize"))
+            {
+                GameObject activeGameObject = Selection.activeGameObject;
+                if (activeGameObject)
+                {
+                    MeshRenderer renderer = activeGameObject.GetComponent<MeshRenderer>();
+                    renderer.sharedMaterial.color = _color;
+                }
+            }
+        }
+    }
+}
