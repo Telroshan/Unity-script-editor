74f88b3084a4a35711c4a579cd1b39a61cb04271
Vincent <vichenzo-thebaud@hotmail.com>
2019-10-31 13:55:29 +0100

    Scaler

---
+++ Assets/Scripts/Editor/ScalerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(Scaler))]
+    public class ScalerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            Scaler scaler = (Scaler) target;
+            EditorGUILayout.LabelField("Oscillates around a base size");
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(Scaler.size)));
+            scaler.transform.localScale = new Vector3(scaler.size, scaler.size, scaler.size);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Scaler : MonoBehaviour
+{
+    [Range(0f, 2f)] public float size = 1f;
+    
+    void Update()
+    {
+        float anim = size + Mathf.Sin(Time.time * 8f) * size / 7f;
+        transform.localScale = Vector3.one * anim;
+    }
+}
