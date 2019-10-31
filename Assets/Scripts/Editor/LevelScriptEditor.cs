6d278880567fa64c58d5aaffb9f957f002dd7628
Vincent <vichenzo-thebaud@hotmail.com>
2019-10-31 12:18:44 +0100

    Level script

---
+++ Assets/Scripts/Editor/LevelScriptEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    [CustomEditor(typeof(LevelScript))]
+    public class LevelScriptEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            LevelScript level = (LevelScript) target;
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(LevelScript.experience)));
+            EditorGUILayout.LabelField("Level", level.Level.ToString());
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class LevelScript : MonoBehaviour
+{
+    public int experience;
+    public int Level => experience / 750;
+}
