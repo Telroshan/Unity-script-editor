3048492ccd7d21d9d733507d8c355fc188baa7d4
Telroshan <vincent.thebaud@ynov.com>
2019-11-04 18:32:07 +0100

    Split code + some comments

--- Assets/ScriptEditor/Editor/ScriptEditor.cs
+++ Assets/ScriptEditor/Editor/ScriptEditor.cs
@@ -19,8 +19,12 @@ namespace ScriptEditor.Editor
         private Color _feedbackColor;
         private Color DefaultLabelColor => EditorStyles.label.normal.textColor;
 
+        private GUIStyle _buttonStyle;
+        private GUIStyle _editButtonStyle;
+
         private void OnEnable()
         {
+            // Retrieve script's content
             MonoBehaviour script = (MonoBehaviour) target;
             _monoScript = MonoScript.FromMonoBehaviour(script);
             _scriptContent = _monoScript.text;
@@ -29,8 +33,67 @@ namespace ScriptEditor.Editor
         private GUIContent GetButtonGuiContent(string iconName, string text, string tooltip)
         {
             GUIContent guiContent = new GUIContent(EditorGUIUtility.IconContent(iconName))
-                {text = text, tooltip = tooltip};
+            {
+                text = text,
+                tooltip = tooltip,
+            };
             return guiContent;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            SetupStyles();
+
+            DisplayEditButton();
+
+            if (!_displayed) return;
+
+            DisplayCodeArea();
+            DisplayFeedbackMessage();
+
+            EditorGUILayout.BeginHorizontal();
+            DisplaySaveButton();
+            DisplayDiscardButton();
+            DisplayHighlightButton();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void SetupStyles()
+        {
+            _editButtonStyle = new GUIStyle(EditorStyles.miniButton)
+            {
+                fixedHeight = 50f,
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 20,
+            };
+            _buttonStyle = new GUIStyle(EditorStyles.miniButton)
+            {
+                fixedHeight = 30f,
+                fontSize = 11,
+            };
+        }
+
+        private void DisplayEditButton()
+        {
+            if (!_displayed &&
+                GUILayout.Button(
+                    GetButtonGuiContent("d_editicon.sml", "Edit",
+                        "Click here to be able to edit your script directly in this inspector"), _editButtonStyle))
+            {
+                SetFeedbackMessage(null, DefaultLabelColor);
+                _displayed = true;
+            }
+        }
+
+        private void DisplayCodeArea()
+        {
+            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos,
+                GUILayout.Width(EditorGUIUtility.currentViewWidth - 25),
+                GUILayout.MaxHeight(300));
+            _scriptContent = EditorGUILayout.TextArea(_scriptContent, GUILayout.ExpandHeight(true));
+            EditorGUILayout.EndScrollView();
         }
 
         private void SetFeedbackMessage(string value, Color color)
@@ -39,46 +102,27 @@ namespace ScriptEditor.Editor
             _feedbackColor = color;
         }
 
-        public override void OnInspectorGUI()
+        private void DisplayFeedbackMessage()
         {
-            base.OnInspectorGUI();
-
-            GUIStyle editButtonStyle = new GUIStyle(EditorStyles.miniButton)
-            {
-                fixedHeight = 50f,
-                alignment = TextAnchor.MiddleCenter,
-                fontSize = 20,
-            };
-            if (!_displayed &&
-                GUILayout.Button(
-                    GetButtonGuiContent("d_editicon.sml", "Edit",
-                        "Click here to be able to edit your script directly in this inspector"), editButtonStyle))
-            {
-                SetFeedbackMessage(null, DefaultLabelColor);
-                _displayed = true;
-            }
-
-            if (!_displayed) return;
-
-            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos,
-                GUILayout.Width(EditorGUIUtility.currentViewWidth - 25),
-                GUILayout.MaxHeight(300));
-            _scriptContent = EditorGUILayout.TextArea(_scriptContent, GUILayout.ExpandHeight(true));
-            EditorGUILayout.EndScrollView();
-
             EditorGUILayout.LabelField(_feedbackMessage,
                 new GUIStyle(EditorStyles.label) {wordWrap = true, normal = {textColor = _feedbackColor}});
+        }
 
-            GUIStyle buttonStyle = new GUIStyle(EditorStyles.miniButton) {fixedHeight = 30f, fontSize = 11};
-
-            EditorGUILayout.BeginHorizontal();
+        private void DisplaySaveButton()
+        {
             if (GUILayout.Button(GetButtonGuiContent("SaveActive", "Save", "Apply your changes to the script"),
-                buttonStyle))
+                _buttonStyle))
             {
-                string guid = AssetDatabase.FindAssets(target.GetType().Name).FirstOrDefault();
-                string path = AssetDatabase.GUIDToAssetPath(guid);
-                File.WriteAllBytes(path, Encoding.UTF8.GetBytes(_scriptContent));
+                string scriptGuid = AssetDatabase.FindAssets(target.GetType().Name).FirstOrDefault();
+                string scriptPath = AssetDatabase.GUIDToAssetPath(scriptGuid);
+
+                // Write new content into the script file
+                File.WriteAllBytes(scriptPath, Encoding.UTF8.GetBytes(_scriptContent));
+
+                // Refresh to force recompile
                 AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+
+                // Intercept logs to display a feedback
                 Application.logMessageReceived += (condition, trace, type) =>
                 {
                     switch (type)
@@ -93,23 +137,32 @@ namespace ScriptEditor.Editor
                     }
                 };
             }
+        }
 
+        private void DisplayDiscardButton()
+        {
             if (GUILayout.Button(GetButtonGuiContent("d_TreeEditor.Trash", "Discard",
-                "Reverts your changes back to the original script"), buttonStyle))
+                "Reverts your changes back to the original script"), _buttonStyle))
             {
+                // Get back to the original content
                 _scriptContent = _monoScript.text;
+
+                // Focus out the text area
                 EditorGUI.FocusTextInControl(null);
+
                 SetFeedbackMessage("Changes discarded", DefaultLabelColor);
             }
+        }
 
+        private void DisplayHighlightButton()
+        {
             if (GUILayout.Button(GetButtonGuiContent("ViewToolZoom", "Select",
-                "Highlight the script in the project window"), buttonStyle))
+                "Highlight the script in the project window"), _buttonStyle))
             {
                 EditorGUIUtility.PingObject(_monoScript);
+
                 SetFeedbackMessage("Script Highlighted", DefaultLabelColor);
             }
-
-            EditorGUILayout.EndHorizontal();
         }
     }
 }
